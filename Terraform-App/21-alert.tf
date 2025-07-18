227ed8f8b5d20f2f776e635a1cdf7adbb6ee6f4c
lsh <lsh@gmail.com>
2025-07-18 16:04:13 +0900

    Jenkins fix

--- Terraform-App/21-alert.tf
+++ Terraform-App/21-alert.tf
@@ -1,155 +1,158 @@
-variable "grafana_api_key" {
-  type      = string
-  sensitive = true
-}
+# variable "grafana_api_key" {
+#   type      = string
+#   sensitive = true
+# }
 
-provider "grafana" {
-  url  = "http://k8s-monitori-grafanai-da2b011f10-124882021.ap-northeast-2.elb.amazonaws.com"
-  auth = var.grafana_api_key
-}
+# provider "grafana" {
+#   url  = "http://k8s-monitori-grafanai-da2b011f10-124882021.ap-northeast-2.elb.amazonaws.com"
+#   auth = var.grafana_api_key
+# }
 
-resource "grafana_data_source" "prometheus" {
-  name = "Prometheus"
-  type = "prometheus"
-  url  = "http://prometheus-server.monitoring.svc.cluster.local:80" # ← 클러스터 내 주소
+# resource "grafana_data_source" "prometheus" {
+#   name = "Prometheus"
+#   type = "prometheus"
+#   url  = "http://prometheus-server.monitoring.svc.cluster.local:80" # ← 클러스터 내 주소
 
-  json_data = {
-    time_interval = "30s"
-  }
-}
+#   json_data = {
+#     time_interval = "30s"
+#   }
+# }
 
-resource "grafana_contact_point" "discord" {
-  name = "discord-contact"
-  type = "discord"
+# resource "grafana_contact_point" "discord" {
+#   name = "discord-contact"
+#   type = "discord"
 
-  settings = jsonencode({
-    url = "https://discord.com/api/webhooks/1392319208339144847/H2XrOcmDERVVOSX45sHuPSmkg_ZZ1AEym8E62WhufAjPIQZ6u45G5kGmmkkasqoz6W20"
-  })
-}
+#   settings = jsonencode({
+#     url = "https://discord.com/api/webhooks/1392319208339144847/H2XrOcmDERVVOSX45sHuPSmkg_ZZ1AEym8E62WhufAjPIQZ6u45G5kGmmkkasqoz6W20"
+#   })
+# }
 
-resource "grafana_folder" "my_folder" {
-  title = "My Rule Folder"
-}
+# resource "grafana_folder" "my_folder" {
+#   title = "My Rule Folder"
+# }
 
-resource "grafana_rule_group" "my_alert_rule_group" {
-  name              = "My Alert Rules"
-  folder_uid        = grafana_folder.my_folder.uid
-  interval_seconds  = 60
-  org_id            = 1
+# resource "grafana_rule_group" "my_alert_rule_group" {
+#   name              = "My Alert Rules"
+#   folder_uid        = grafana_folder.my_folder.uid
+#   interval_seconds  = 60
+#   org_id            = 1
 
-  rule {
-    name      = "My Random Walk Alert"
-    condition = "C"
-    for       = "0s"
+#   rule {
+#     name      = "My Random Walk Alert"
+#     condition = "C"
+#     for       = "0s"
 
-    data {
-      ref_id = "A"
-      relative_time_range {
-        from = 600
-        to   = 0
-      }
-      datasource_uid = grafana_data_source.prometheus.uid
-      model = jsonencode({
-        intervalMs     = 1000
-        maxDataPoints  = 43200
-        refId          = "A"
-      })
-    }
+#     data {
+#       ref_id = "A"
+#       relative_time_range {
+#         from = 600
+#         to   = 0
+#       }
+#       datasource_uid = grafana_data_source.prometheus.uid
+#       model = jsonencode({
+#         intervalMs     = 1000
+#         maxDataPoints  = 43200
+#         refId          = "A"
+#       })
+#     }
 
-    data {
-      datasource_uid = "__expr__"
-      ref_id         = "B"
-      relative_time_range {
-        from = 0
-        to   = 0
-      }
-      model = <<EOT
-{
-  "conditions": [{
-    "evaluator": { "params": [70], "type": "gt" },
-    "operator": { "type": "and" },
-    "query": { "params": ["A"] },
-    "reducer": { "type": "last" },
-    "type": "query"
-  }],
-  "datasource": {
-    "name": "Expression",
-    "type": "__expr__",
-    "uid": "__expr__"
-  },
-  "expression": "A",
-  "hide": false,
-  "intervalMs": 1000,
-  "maxDataPoints": 43200,
-  "refId": "B",
-  "type": "reduce"
-}
-EOT
-    }
+#     data {
+#       datasource_uid = "__expr__"
+#       ref_id         = "B"
+#       relative_time_range {
+#         from = 0
+#         to   = 0
+#       }
+#       model = <<EOT
+# {
+#   "conditions": [{
+#     "evaluator": { "params": [70], "type": "gt" },
+#     "operator": { "type": "and" },
+#     "query": { "params": ["A"] },
+#     "reducer": { "type": "last" },
+#     "type": "query"
+#   }],
+#   "datasource": {
+#     "name": "Expression",
+#     "type": "__expr__",
+#     "uid": "__expr__"
+#   },
+#   "expression": "A",
+#   "hide": false,
+#   "intervalMs": 1000,
+#   "maxDataPoints": 43200,
+#   "refId": "B",
+#   "type": "reduce"
+# }
+# EOT
+#     }
 
-    data {
-      datasource_uid = "__expr__"
-      ref_id         = "C"
-      relative_time_range {
-        from = 0
-        to   = 0
-      }
-      model = jsonencode({
-        expression = "$B > 70"
-        type       = "math"
-        refId      = "C"
-      })
-    }
-  }
-}
+#     data {
+#       datasource_uid = "__expr__"
+#       ref_id         = "C"
+#       relative_time_range {
+#         from = 0
+#         to   = 0
+#       }
+#       model = jsonencode({
+#         expression = "$B > 70"
+#         type       = "math"
+#         refId      = "C"
+#       })
+#     }
+#   }
+# }
 
-resource "grafana_rule_group" "pod_alert_rules" {
-  name              = "Pod Alert Rules"
-  folder_uid        = grafana_folder.my_folder.uid
-  interval_seconds  = 60
-  org_id            = 1
+# resource "grafana_rule_group" "pod_alert_rules" {
+#   name              = "Pod Alert Rules"
+#   folder_uid        = grafana_folder.my_folder.uid
+#   interval_seconds  = 60
+#   org_id            = 1
 
-  rule {
-    name = "Pod frontend 죽음"
-    condition = "A"
-    for       = "0s"
+#   rule {
+#     name = "Pod frontend 죽음"
+#     condition = "A"
+#     for       = "0s"
 
-    data {
-      ref_id = "A"
-      datasource_uid = grafana_data_source.prometheus.uid
-      relative_time_range {
-        from = 60
-        to   = 0
-      }
-      model = jsonencode({
-        expr = "absent(kube_pod_status_phase{namespace=\"default\", pod=~\"frontend-.*\", phase=\"Running\"})"
-        interval = ""
-        legendFormat = "frontend pod down"
-        refId = "A"
-      })
-    }
-  }
+#     data {
+#       ref_id = "A"
+#       datasource_uid = grafana_data_source.prometheus.uid
+#       relative_time_range {
+#         from = 60
+#         to   = 0
+#       }
+#       model = jsonencode({
+#         expr = "absent(kube_pod_status_phase{namespace=\"default\", pod=~\"frontend-.*\", phase=\"Running\"})"
+#         interval = ""
+#         legendFormat = "frontend pod down"
+#         refId = "A"
+#       })
+#     }
+#   }
 
-  rule {
-    name = "Pod frontend 살아있음"
-    condition = "A"
-    for       = "0s"
+#   rule {
+#     name = "Pod frontend 살아있음"
+#     condition = "A"
+#     for       = "0s"
 
-    data {
-      ref_id = "A"
-      datasource_uid = grafana_data_source.prometheus.uid
-      relative_time_range {
-        from = 60
-        to   = 0
-      }
-      model = jsonencode({
-        expr = "kube_pod_status_phase{namespace=\"default\", pod=~\"frontend-.*\", phase=\"Running\"} > 0"
-        interval = ""
-        legendFormat = "frontend pod alive"
-        refId = "A"
-      })
-    }
-  }
+#     data {
+#       ref_id = "A"
+#       datasource_uid = grafana_data_source.prometheus.uid
+#       relative_time_range {
+#         from = 60
+#         to   = 0
+#       }
+#       model = jsonencode({
+#         expr = "kube_pod_status_phase{namespace=\"default\", pod=~\"frontend-.*\", phase=\"Running\"} > 0"
+#         interval = ""
+#         legendFormat = "frontend pod alive"
+#         refId = "A"
+#       })
+#     }
+#   }
 
-  # 아래는 boards, users 알림 4개 추가 필요 (복사 붙여넣기해서 pod 이름만 바꾸면 됨)
-}
+#   # 아래는 boards, users 알림 4개 추가 필요 (복사 붙여넣기해서 pod 이름만 바꾸면 됨)
+# }
+
+
+## 프로메테우스도 ALB로 열어놔야가능
--- Terraform-Infra/17-Jenkins.tf
+++ Terraform-Infra/17-Jenkins.tf
@@ -28,7 +28,6 @@ resource "aws_instance" "jenkins" {
               EOF
 }
 
-
 resource "aws_iam_role" "jenkins_ecr_role" {
   name = "jenkins-ecr-access"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
@@ -71,56 +70,9 @@ variable "terraform_values_repo" {
 }
 
 
-pipeline {
-    agent any
-    environment {
-        AWS_REGION = "ap-northeast-2"
-        REPO = "https://github.com/lsh5224/Terraform_test.git"
-    }
-    stages {
-        stage('Clone Repo') {
-            steps {
-                sh 'git clone $REPO'
-            }
-        }
-        stage('Fetch ECR Tags') {
-            steps {
-                script {
-                    def repos = ["frontend", "backend-user", "backend-boards"]
-                    def latestTags = [:]
-                    for (repo in repos) {
-                        def tags = sh(script: "aws ecr list-images --repository-name ${repo} --query 'imageIds[*].imageTag' --output text", returnStdout: true).trim().split()
-                        latestTags[repo] = tags.sort().last()
-                    }
-                    writeFile file: 'Terraform_test/values.yaml', text: updateYamlWithTags(latestTags)
-                }
-            }
-        }
-        stage('Git Push') {
-            steps {
-                dir('Terraform_test') {
-                    sh '''
-                    git config --global user.email "jenkins@ci"
-                    git config --global user.name "Jenkins CI"
-                    git add .
-                    git commit -m "Update image tags"
-                    git push origin main
-                    '''
-                }
-            }
-        }
-    }
-}
 
-def updateYamlWithTags(tags) {
-    return """
-backend:
-  boards:
-    tag: "${tags["backend-boards"]}"
-  user:
-    tag: "${tags["backend-user"]}"
 
-frontend:
-  tag: "${tags["frontend"]}"
-"""
-}
+
+
+
+
