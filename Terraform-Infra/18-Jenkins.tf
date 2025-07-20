7f7015cd1d09477d7cb49ca69f84769637456f14
lsh <lsh@gmail.com>
2025-07-21 08:08:57 +0900

    1st

---
+++ Terraform-Infra/18-Jenkins.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "jenkins" {
+  ami                    = "ami-056a29f2eddc40520"
+  instance_type          = "t3.medium"
+  subnet_id              = aws_subnet.MSA_pub_subnet_2a.id
+  vpc_security_group_ids = [aws_security_group.jenkins_sg]
+  key_name               = "ja-01"
+
+  user_data = file("${path.module}/scripts/init_jenkins.sh")
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
