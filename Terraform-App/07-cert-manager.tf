3e83a10019da2a28597820e7769b5db28b5c412c
lsh <lsh@gmail.com>
2025-07-22 09:47:43 +0900

    force fix

--- Terraform-App/07-cert-manager.tf
+++ Terraform-App/07-cert-manager.tf
@@ -1,12 +1,16 @@
 resource "helm_release" "cert_manager" {
-  name             = "cert-manager"
-  repository       = "https://charts.jetstack.io"
-  chart            = "cert-manager"
-  namespace        = "cert-manager"
-  depends_on = [kubernetes_namespace.cert-manager]
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  namespace  = "cert-manager"
+  version    = "v1.14.4"
+  create_namespace = true
 
   set {
     name  = "installCRDs"
     value = "false"
   }
+
+  wait    = false
+  timeout = 600
 }
