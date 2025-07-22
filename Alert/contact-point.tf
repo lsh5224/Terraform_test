e208e422f3a1e50be3f4768bdbfbf3f33660dd39
lsh <lsh@gmail.com>
2025-07-22 17:02:52 +0900

    fix

--- Alert/contact-point.tf
+++ Alert/contact-point.tf
@@ -6,8 +6,4 @@ resource "grafana_contact_point" "discord_point" {
     disable_resolve_message = false
   }
 
-  depends_on = [
-  grafana_rule_group.app_alerts,
-  grafana_notification_policy.default_policy,
-  ]
 }
