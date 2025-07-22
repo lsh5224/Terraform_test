488079269863ff4f39ee6a189e7ef5250d0abb8b
lsh <lsh@gmail.com>
2025-07-22 17:39:19 +0900

    123123

--- Alert/main.tf
+++ Alert/main.tf
@@ -4,7 +4,12 @@ terraform {
       source  = "grafana/grafana"
       version = ">= 3.0.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
+    }
   }
+  
 }
 
 provider "grafana" {
