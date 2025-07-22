c5f2878cba8e8074eda1cc9c4ac90134ffff9c1f
lsh <lsh@gmail.com>
2025-07-22 11:51:31 +0900

    2

--- Alert/datasource.tf
+++ Alert/datasource.tf
@@ -1,10 +1,12 @@
 resource "grafana_data_source" "prometheus" {
   name   = "Prometheus"
   type   = "prometheus"
-  access = "proxy"
+  access_mode = "proxy"
   url    = "http://prometheus-operated.monitoring.svc.cluster.local:9090"
 
-  json_data = {
-    time_interval = "30s"
-  }
+  # json_data 대신 json_data_encoded 로 넘기세요
+  json_data_encoded = jsonencode({
+    # JSON 키 이름은 Grafana API 스펙에 맞춰 camelCase
+    timeInterval = "30s"
+  })
 }
