e496f8415c790e5b519b288b7e01fe4595eda94e
lsh <lsh@gmail.com>
2025-07-22 11:33:58 +0900

    fix policy

--- Alert/notification_policy.tf
+++ Alert/notification_policy.tf
@@ -1,11 +1,11 @@
 resource "grafana_notification_policy" "default_policy" {
-  # 전체 정책 트리의 기본 contact point
+  # 알림을 묶을 레이블 키 (일반적으로 alertname 사용)
+  group_by      = ["alertname"]
+  # Default route의 Contact Point로 Discord-point 지정
   contact_point = grafana_contact_point.discord_point.name
 
-  policy {
-    matcher {
-      action = "default"
-    }
-    contact_point = grafana_contact_point.discord_point.name
-  }
+  # (선택) 그룹화 및 반복 타이밍을 조정하려면 아래 옵션 추가
+  # group_wait     = "30s"
+  # group_interval = "5m"
+  # repeat_interval = "1h"
 }
